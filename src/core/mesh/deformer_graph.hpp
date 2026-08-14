e124c823aeb8527a7cb7344bc63d1b7bfb748917
rmky <ryoooyu@gmail.com>
2026-08-14 12:40:45 +0900

    mesh: expose runtime deformer parameter updates

--- src/core/mesh/deformer_graph.hpp
+++ src/core/mesh/deformer_graph.hpp
@@ -19,6 +19,8 @@ class DeformerGraph {
 public:
     void add(DeformerNode node);
     void clear();
+    bool set_weight(const std::string &id, float weight);
+    bool set_transform(const std::string &id, RegionTransform transform);
     Mesh evaluate(const Mesh &source) const;
     std::size_t size() const { return nodes_.size(); }
 
